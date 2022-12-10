ce9187e9babfd1c1232cae1e5ad6b210a7f75a9e
GustavoBonif <gubonic@gmail.com>
2022-12-09 22:10:00 -0300

    001 - Criando cabecalho

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<script setup>
+    // 
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg bg-warning">
+        <div class="container-fluid">
+            <div class="navbar-brand">
+                <img class="header-logo" src="@/assets/images/logo-aumigo.png" alt="#">
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item badge badge-light">
+                        <button class="btn btn-light">
+                            <a class="nav-link" aria-current="page" href="#">Home</a>
+                        </button>
+                    </li>
+                    <li class="nav-item badge">
+                        <button class="btn btn-light">
+                            <a class="nav-link" aria-current="page" href="#">Cachorros</a>
+                        </button>
+                    </li>
+                </ul>
+
+                <div class="btn-group">
+                    <a href="#" class="btn btn-secondary">Entrar</a>
+                    <a href="#" class="btn btn-secondary">Registrar-se</a>
+                </div>
+            </div>
+            
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+.header-logo {
+    width: 60%;
+    margin-left: 10%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const app = createApp(App)
 
