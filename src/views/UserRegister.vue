5d30cc6ae9592b13d89ce003a6ac47266b09de05
Leticia dos Santos <36967995+LeticiaDosSantos@users.noreply.github.com>
2022-12-15 10:10:07 -0300

    Tela de Cadastro de Usuário
    - Inclusão de título no topo da página
    - Inclusão de placeholder nos campos para tornar mais intuitivo
    - Alteração na mensagem a respeito da confirmação de senha

--- src/views/UserRegister.vue
+++ src/views/UserRegister.vue
@@ -1,19 +1,28 @@
 <template>
     <form class="m-auto w-50 pt-3">
-        <h2 class="text-center">Cadastrar-se</h2>
+        <hr>
+        <h2 class="text-center text-warning">Cadastre-se</h2>
+        <hr>
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Seu e-mail</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <label for="exampleInputEmail1" class="form-label">E-mail</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="exemplo@gmail.com">
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Senha</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="*********">
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Confirmar senha</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-            <div id="emailHelp" class="form-text">Informe a mesma senha do campo acima. </div>
+            <div class="row">
+                <div class="col">
+                    <label for="exampleInputPassword1" class="form-label">Confirmar senha</label>
+                </div>
+                <div class="col">
+                    <div id="emailHelp" class="form-text text-danger">* Informe a mesma senha do campo acima. 
+                </div>
+            </div>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="*********">
         </div>
-        <button type="submit" class="btn btn btn-outline-success">Enviar</button>
+        </div>
+        <button type="submit" class="btn btn btn-outline-success">Cadastrar</button>
     </form>
 </template>
