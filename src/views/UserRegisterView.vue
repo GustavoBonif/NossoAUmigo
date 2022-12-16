486af90d0c07669819dd6e121701827416e716ee
GustavoBonif <gubonic@gmail.com>
2022-12-15 23:10:19 -0300

    Criando campo 'nome de usuário' e padronizando IDs

--- src/views/UserRegisterView.vue
+++ src/views/UserRegisterView.vue
@@ -4,22 +4,32 @@
         <h2 class="text-center text-warning">Cadastre-se</h2>
         <hr>
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">E-mail</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="exemplo@gmail.com">
+            <div class="row">
+                <label for="userName" class="form-label">Nome</label>
+                <input type="text" class="form-control" id="userName" placeholder="Exemplo: João da Silva">
+            </div>
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Senha</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="*********">
+            <div class="row">
+                <label for="userEmail" class="form-label">E-mail</label>
+                <input type="email" class="form-control" id="userEmail" aria-describedby="emailHelp" placeholder="exemplo@gmail.com">
+            </div>
+        </div>
+        <div class="mb-3">
+            <div class="row">
+                <label for="userPassword" class="form-label">Senha</label>
+                <input type="password" class="form-control" id="userPassword" placeholder="*********">
+            </div>
         </div>
         <div class="mb-3">
             <div class="row">
                 <div class="col">
-                    <label for="exampleInputPassword1" class="form-label">Confirmar senha</label>
+                    <label for="userPassword" class="form-label">Confirmar senha</label>
                 </div>
                 <div class="col">
                     <div id="emailHelp" class="form-text text-danger">* Informe a mesma senha do campo acima.</div>
                 </div>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="*********">
+                <input type="password" class="form-control" id="userPassword" placeholder="*********">
             </div>
         </div>
         <button type="submit" class="btn btn btn-outline-success">Cadastrar</button>
