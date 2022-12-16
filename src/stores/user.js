8c69fc656fd4860f4bdcf122aab3d2508e4825c1
GustavoBonif <gubonic@gmail.com>
2022-12-16 00:56:05 -0300

    criando interface JS para cadastro de usuários

---
+++ src/stores/user.js
@@ -0,0 +1,29 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+
+export const useUserStore = defineStore({
+  id: "user",
+  state: () => ({
+    users: [],
+  }),
+  actions: {
+    async saveUser(user) {
+        return await this.addUser(user);
+      },
+    async addUser(user) {
+      try {
+
+        const { data } = await axios.post(
+          "http://localhost:4000/users",
+          user
+        );
+
+        this.users.push(data);
+        return Promise.resolve("Usuário cadastrado com sucesso!");
+      } catch (e) {
+        console.error(e);
+        return Promise.reject(e);
+      }
+    },
+  },
+});
